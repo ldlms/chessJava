0ec4ecec3e22d1f51b8fa42bf0d375b120a2dd22
Leo <leodelmas31@gmail.com>
2024-03-11 23:16:36 +0100

    rook

--- src/com/chess/engine/pieces/Rook.java
+++ src/com/chess/engine/pieces/Rook.java
@@ -5,9 +5,13 @@ import java.util.Collection;
 import java.util.List;
 
 import com.chess.engine.Alliance;
+import com.chess.engine.board.AttackMove;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.MajorMove;
 import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+import com.google.common.collect.ImmutableList;
 
 public final class Rook extends Piece {
 
@@ -18,15 +22,41 @@ public final class Rook extends Piece {
 	}
 
 	@Override
-	public Collection<Move> calculateLegalMoves(Board board) {
+	public Collection<Move> calculateLegalMoves(final Board board) {
 		final List<Move> legalMoves = new ArrayList<>();
 
 		for (final int candidateCoordinate : CANDIDATE_MOVE_VECTOR_COORDINATES) {
 			int pieceCoordinate = this.piecePosition;
 			while (BoardUtils.isValidTileCoordinate(pieceCoordinate)) {
+				if (!firstColumnException(pieceCoordinate, candidateCoordinate)
+						|| !eighthColumnException(pieceCoordinate, candidateCoordinate)) {
+					break;
+				}
+				pieceCoordinate += candidateCoordinate;
+				if (BoardUtils.isValidTileCoordinate(pieceCoordinate)) {
+					final Tile TileDestination = board.getTile(pieceCoordinate);
+					if (!TileDestination.isTileOccupied()) {
+						legalMoves.add(new MajorMove(board, this, pieceCoordinate));
+					} else {
+						final Piece pieceAtDestination = TileDestination.getPiece();
+						final Alliance pieceAlliance = pieceAtDestination.getAlliance();
+						if (this.pieceAlliance != pieceAlliance) {
+							legalMoves.add(new AttackMove(board, this, pieceAtDestination, pieceCoordinate));
+						}
+						break;
+					}
 
+				}
 			}
 		}
-		return null;
+		return ImmutableList.copyOf(legalMoves);
+	}
+
+	private static boolean firstColumnException(final int currentPosition, final int candidateOffset) {
+		return BoardUtils.FIRST_COLUMN[currentPosition] && candidateOffset == -1;
+	}
+
+	private static boolean eighthColumnException(final int currentPosition, final int candidateOffset) {
+		return BoardUtils.EIGHTH_COLUMN[currentPosition] && candidateOffset == 1;
 	}
 }
