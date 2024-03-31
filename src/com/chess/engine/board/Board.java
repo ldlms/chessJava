1014e46b84bcad845f81ea1fd118ca5fb2926cb7
Leo <leodelmas31@gmail.com>
2024-03-31 14:01:48 +0200

    toString

--- src/com/chess/engine/board/Board.java
+++ src/com/chess/engine/board/Board.java
@@ -25,9 +25,39 @@ public class Board {
 		this.gameBoard = createGameBoard(builder);
 		this.whitePieces = calculateActivePieces(this.gameBoard, Alliance.WHITE);
 		this.blackPieces = calculateActivePieces(this.gameBoard, Alliance.BLACK);
+
+		final Collection<Move> whiteStandardLegalMove = calculateLegalMoves(this.whitePieces);
+		final Collection<Move> blackStandardLegalMove = calculateLegalMoves(this.blackPieces);
 	}
 
-	private Collection<Piece> calculateActivePieces(final List<Tile> gameBoard, final Alliance alliance) {
+	@Override
+	public String toString() {
+		final StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < BoardUtils.NUM_TILES; i++) {
+			final String tileText = prettyPrint(this.gameBoard.get(i));
+			builder.append(String.format("%3s", tileText));
+			if ((i + 1) % BoardUtils.NUM_TILES_PER_ROW == 0) {
+				builder.append("\n");
+			}
+		}
+		return builder.toString();
+	}
+
+	private static String prettyPrint(Tile tile) {
+
+		return null;
+	}
+
+	private Collection<Move> calculateLegalMoves(final Collection<Piece> pieces) {
+
+		final List<Move> legalMoves = new ArrayList<>();
+		for (final Piece piece : pieces) {
+			legalMoves.addAll(piece.calculateLegalMoves(this));
+		}
+		return ImmutableList.copyOf(legalMoves);
+	}
+
+	private static Collection<Piece> calculateActivePieces(final List<Tile> gameBoard, final Alliance alliance) {
 
 		final List<Piece> activePieces = new ArrayList<>();
 
