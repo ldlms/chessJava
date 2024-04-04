a8a92513847b54ec37fc3afe474c3416d8f249f9
Leo <leodelmas31@gmail.com>
2024-04-04 11:13:10 +0200

    moveTransition

--- src/com/chess/engine/player/MoveTransition.java
+++ src/com/chess/engine/player/MoveTransition.java
@@ -1,5 +1,17 @@
 package com.chess.engine.player;
 
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+
 public class MoveTransition {
 
+	private final Board TransitionBoard;
+	private final Move move;
+	private final MoveStatus movestatus;
+
+	public MoveTransition(final Board board, final Move move, final MoveStatus movestatus) {
+		this.TransitionBoard = board;
+		this.move = move;
+		this.movestatus = movestatus;
+	}
 }
