7db51cf69d26aa670f126aa5aa75d5c960fc7a6e
Leo <leodelmas31@gmail.com>
2024-05-11 16:55:22 +0200

    castle

--- src/com/chess/engine/board/CastleMove.java
+++ src/com/chess/engine/board/CastleMove.java
@@ -1,5 +1,6 @@
 package com.chess.engine.board;
 
+import com.chess.engine.board.Board.Builder;
 import com.chess.engine.pieces.Piece;
 import com.chess.engine.pieces.Rook;
 
@@ -16,10 +17,41 @@ abstract class CastleMove extends Move {
 		this.castleRookStart = castleRookStart;
 	}
 
+	@Override
+	public boolean isCastlingMove() {
+		return true;
+	}
+
+	@Override
+	public Board execute() {
+		final Builder builder = new Builder();
+		for (final Piece piece : this.board.currentPlayer().getActivePiece()) {
+			if (!this.movedPiece.equals(piece) && !this.castleRook.equals(piece)) {
+				builder.setPiece(piece);
+			}
+		}
+		for (final Piece piece : this.board.currentPlayer().getOpponent().getActivePiece()) {
+			builder.setPiece(piece);
+		}
+		builder.setPiece(this.movedPiece.movePiece(this));
+		builder.setPiece(new Rook(this.castleRook.getAlliance(), this.castleRookDestination));
+		builder.setMoveMaker(this.board.currentPlayer().getOpponent().getAlliance());
+		return builder.build();
+	}
+
+	public Rook getCastleRook() {
+		return this.castleRook;
+	}
+
 	public static final class KingSideCastleMove extends CastleMove {
 		public KingSideCastleMove(final Board board, final Piece movedPiece, final int destination,
 				final Rook castleRook, final int castleRookDestination, final int castleRookStart) {
 			super(board, movedPiece, destination, castleRook, castleRookStart, castleRookDestination);
+		}
+
+		@Override
+		public String toString() {
+			return "0-0";
 		}
 	}
 
@@ -27,6 +59,11 @@ abstract class CastleMove extends Move {
 		public QueenSideCastleMove(final Board board, final Piece movedPiece, final int destination,
 				final Rook castleRook, final int castleRookDestination, final int castleRookStart) {
 			super(board, movedPiece, destination, castleRook, castleRookStart, castleRookDestination);
+		}
+
+		@Override
+		public String toString() {
+			return "0-0-0";
 		}
 	}
 
