88001ca50108ed52ba0c492b150be4ce825d93fc
Leo <leodelmas31@gmail.com>
2024-05-24 20:06:48 +0200

    gui

--- src/com/chess/engine/board/BoardUtils.java
+++ src/com/chess/engine/board/BoardUtils.java
@@ -13,7 +13,7 @@ public class BoardUtils {
 	public static final boolean[] SECOND_ROW = initRow(8);
 	public static final boolean[] SEVENTH_ROW = initRow(48);
 
-	static final int NUM_TILES = 64;
+	public static final int NUM_TILES = 64;
 	public static final int NUM_TILES_PER_ROW = 8;
 
 	private static boolean[] initColumn(int columnNumber) {
