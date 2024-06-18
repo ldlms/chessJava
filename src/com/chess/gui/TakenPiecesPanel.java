31778301f0423fc4c05967d5dea133d41438e349
Leo <leodelmas31@gmail.com>
2024-06-18 20:31:15 +0200

    panel

--- src/com/chess/gui/TakenPiecesPanel.java
+++ src/com/chess/gui/TakenPiecesPanel.java
@@ -1,5 +1,26 @@
 package com.chess.gui;
 
-public class TakenPiecesPanel {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
 
+import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
+
+public class TakenPiecesPanel extends JPanel {
+
+	private final JPanel northPanel;
+	private final JPanel southPanel;
+
+	private static final EtchedBorder PANEL_BORDER = new EtchedBorder(EtchedBorder.RAISED);
+
+	public TakenPiecesPanel() {
+		super(new BorderLayout());
+		setBackground(Color.decode("0xFDF5E"));
+		setBorder(PANEL_BORDER);
+		this.northPanel = new JPanel(new GridLayout(8, 2));
+		this.southPanel = new JPanel(new GridLayout(8, 2));
+		this.northPanel.setBackground(PANEL_COLOR);
+		this.southPanel.setBackground(PANEL_COLOR);
+	}
 }
