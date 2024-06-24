9f7cd9e2b433a02e1613a89e403cbe778a1dcb3f
Leo <leodelmas31@gmail.com>
2024-06-24 16:27:36 +0200

    takenPieces

--- src/com/chess/gui/TakenPiecesPanel.java
+++ src/com/chess/gui/TakenPiecesPanel.java
@@ -4,14 +4,24 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EtchedBorder;
 
+import com.chess.engine.board.Move;
 import com.chess.engine.pieces.Piece;
 import com.chess.gui.Table.MoveLog;
+import com.google.common.primitives.Ints;
 
 public class TakenPiecesPanel extends JPanel {
 
@@ -41,6 +51,56 @@ public class TakenPiecesPanel extends JPanel {
 		this.northPanel.removeAll();
 
 		final List<Piece> whiteTakenPieces = new ArrayList<>();
-		final List<Piece> blackTAkenPieces = new ArrayList<>();
+		final List<Piece> blackTakenPieces = new ArrayList<>();
+
+		for (final Move move : moveLog.getMoves()) {
+			if (move.isAttack()) {
+				final Piece takenPiece = move.getAttackedPiece();
+				if (takenPiece.getAlliance().isBlack()) {
+					blackTakenPieces.add(takenPiece);
+				} else {
+					whiteTakenPieces.add(takenPiece);
+				}
+			}
+		}
+		Collections.sort(whiteTakenPieces, new Comparator<Piece>() {
+			@Override
+			public int compare(Piece o1, Piece o2) {
+				return Ints.compare(o1.getPieceValue(), o2.getPieceValue());
+			}
+		});
+
+		Collections.sort(blackTakenPieces, new Comparator<Piece>() {
+			@Override
+			public int compare(Piece o1, Piece o2) {
+				return Ints.compare(o1.getPieceValue(), o2.getPieceValue());
+			}
+		});
+
+		for (final Piece takenPiece : whiteTakenPieces) {
+			try {
+				final BufferedImage image = ImageIO.read(new File(
+						"art/" + takenPiece.getAlliance().toString().substring(0, 1) + "" + takenPiece.toString()));
+				final ImageIcon icon = new ImageIcon(image);
+				final JLabel imageLabel = new JLabel();
+				this.southPanel.add(imageLabel);
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		for (final Piece takenPiece : blackTakenPieces) {
+			try {
+				final BufferedImage image = ImageIO.read(new File(
+						"art/" + takenPiece.getAlliance().toString().substring(0, 1) + "" + takenPiece.toString()));
+				final ImageIcon icon = new ImageIcon(image);
+				final JLabel imageLabel = new JLabel();
+				this.southPanel.add(imageLabel);
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		validate();
 	}
 }
