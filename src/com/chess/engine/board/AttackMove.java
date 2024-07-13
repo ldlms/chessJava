adfe209a707217c19c63984b546b07e6fc7f2c8d
Leo <leodelmas31@gmail.com>
2024-07-13 20:52:16 +0200

    MajorAttackMove

--- src/com/chess/engine/board/AttackMove.java
+++ src/com/chess/engine/board/AttackMove.java
@@ -39,4 +39,22 @@ public class AttackMove extends Move {
 		return this.attackedPiece;
 	}
 
+	public static class MajorAttackMove extends AttackMove {
+
+		public MajorAttackMove(final Board board, final Piece movedPiece, final int destinationCoordinate,
+				final Piece attackedPiece) {
+			super(board, movedPiece, attackedPiece, destinationCoordinate);
+		}
+
+		@Override
+		public boolean equals(Object other) {
+			return this == other || other instanceof MajorAttackMove && super.equals(other);
+		}
+
+		@Override
+		public String toString() {
+			return movedPiece.getPieceType() + BoardUtils.getPositionAtCoordinate(this.destination);
+		}
+	}
+
 }
