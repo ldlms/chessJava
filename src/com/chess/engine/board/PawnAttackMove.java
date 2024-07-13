053e3529a405a50e4be78010b7a43ec630d26700
Leo <leodelmas31@gmail.com>
2024-07-13 17:38:48 +0200

    toString

--- src/com/chess/engine/board/PawnAttackMove.java
+++ src/com/chess/engine/board/PawnAttackMove.java
@@ -7,4 +7,16 @@ public class PawnAttackMove extends AttackMove {
 	public PawnAttackMove(final Board board, final Piece movedPiece, final Piece attackedPiece, final int coordinate) {
 		super(board, movedPiece, attackedPiece, coordinate);
 	}
+
+	@Override
+	public boolean equals(final Object other) {
+		return this == other || other instanceof PawnAttackMove && super.equals(other);
+	}
+
+	@Override
+	public String toString() {
+		return BoardUtils.getPositionAtCoordinate(this.movedPiece.getPiecePosition()).substring(0, 1) + "x"
+				+ BoardUtils.getPositionAtCoordinate(this.getDestinationCoordinate());
+	}
+
 }
