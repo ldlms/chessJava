d75bfec4ae81f3e65d39a0af79c15749406e1d8b
Leo <leodelmas31@gmail.com>
2024-07-15 17:21:00 +0200

    correctionMoveCandidate

--- src/com/chess/engine/pieces/Knight.java
+++ src/com/chess/engine/pieces/Knight.java
@@ -15,7 +15,7 @@ import com.google.common.collect.ImmutableList;
 
 public final class Knight extends Piece {
 
-	private final static int[] CANDIDATE_MOVE_COORDINATES = { -17, -15, -10, -6, 6, 10, 17 };
+	private final static int[] CANDIDATE_MOVE_COORDINATES = { -17, -15, -10, -6, 6, 10, 15, 17 };
 
 	public Knight(final Alliance pieceAlliance, final int piecePosition) {
 		super(PieceType.KNIGHT, piecePosition, pieceAlliance, true);
